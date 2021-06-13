ebfc79f11ad6daca1848e78ac85c375372aa511a
KoharaKazuya <reflect.results@gmail.com>
2021-06-13 15:45:44 +0900

    feat: Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Header from "../components/header";
+import { pageview } from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useGoogleAnalytics();
+
   return (
     <>
       <Header />
       <Component {...pageProps} />
     </>
   );
+}
+
+function useGoogleAnalytics() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 }
